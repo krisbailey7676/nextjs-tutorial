caddbe3906fa28037410f6ca97613ade2c6bbc0c
Kris Bailey <krisbailey7676@gmail.com>
2024-06-28 18:17:30 -0400

    Creating Auth Sessions.

--- next-app/app/Navbar.tsx
+++ next-app/app/Navbar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <nav className="flex border-b-2 bg-slate-200 space-x-5 mb-5 p-3 h-14 items-center">
       <ul className="flex space-x-5">
@@ -11,6 +16,18 @@ const Navbar = () => {
         <Link href="/admin">Admin</Link>
         <Link href="/users">Users</Link>
         <Link href="/products">Products</Link>
+        {status === "loading" && <div>Loading...</div>}
+        {status === "authenticated" && (
+          <div>
+            {session.user!.name}
+            <Link href="/api/auth/signout" className="ml-3">
+              Sign Out
+            </Link>
+          </div>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
       </ul>
     </nav>
   );
